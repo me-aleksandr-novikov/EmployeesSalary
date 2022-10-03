460c2ca2463822aaffa355be8d55de56187cb03b
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2022-10-03 21:42:10 +1000

    верстка

---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+
+import './app.css';
+
+function App() {
+
+    const data = [
+        {name:'John S.', salary: 800, increase: false},
+        {name:'Alex M.', salary: 3000, increase: true},
+        {name:'Carl W.', salary: 5000, increase: false}
+    ]
+
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+            </div>
+
+            <EmployersList data={data}/>
+            <EmployersAddForm />
+        </div>
+    )
+}
+
+export default App;
