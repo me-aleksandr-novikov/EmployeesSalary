3df29bb4843d5eaed6442bfe38ef58e3e55e78af
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2022-10-07 04:42:59 +1000

    delete/addEmployers

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,15 +8,44 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+                {name:'John S.', salary: 800, increase: false, id:1},
+                {name:'Alex M.', salary: 3000, increase: true, id:2},
+                {name:'Carl W.', salary: 5000, increase: false, id:3}
+            ]
+        };
+        this.maxId = 4;
+    }
 
-    const data = [
-        {name:'John S.', salary: 800, increase: false, id:1},
-        {name:'Alex M.', salary: 3000, increase: true, id:2},
-        {name:'Carl W.', salary: 5000, increase: false, id:3}
-    ]
+    deleteItem = (id) => {
+        this.setState(({data})=>{
+            return {
+                data: data.filter(item => item.id !== id )
+            }
+        })
+    }
+    
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
 
-    return (
+    render() {
+        return(
         <div className="app">
             <AppInfo />
 
@@ -23,10 +54,13 @@ function App() {
                 <AppFilter />
             </div>
 
-            <EmployersList data={data}/>
-            <EmployersAddForm />
+            <EmployersList
+                data={this.state.data}
+                onDelete={this.deleteItem} />
+            <EmployersAddForm onAdd={this.addItem}/>
         </div>
-    )
+        );
+    };
 }
 
 export default App;
--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -3,28 +3,38 @@ import { Component } from 'react';
 import './employees-add-form.css';
 
 class EmployeesAddForm extends Component {
-    constructor (props){
+    constructor(props) {
         super(props);
         this.state = {
-            name: "",
+            name: '',
             salary: ''
         }
     }
-    
+
     onValueChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name] : e.target.value
         })
     }
 
-    render(){
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
+    render() {
         const {name, salary} = this.state;
 
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit = {this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
