83be9c2214bab4b447bfe818e2728ea7309f482f
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2022-10-11 08:58:43 +1000

    m-adaptiv

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -33,23 +33,24 @@ class EmployeesAddForm extends Component {
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex"
+                    className="add-form row mb-3"
                     onSubmit = {this.onSubmit}>
+                    <div>
                     <input type="text"
-                        className="form-control new-post-label"
+                        className=" new-post-label" 
                         placeholder="Как его зовут?"
                         name="name"
                         value={name} 
                         onChange={this.onValueChange}/>
                     <input type="number"
-                        className="form-control new-post-label"
+                        className="new-post-label"
                         placeholder="З/П в $?"
                         name="salary"
                         value={salary} 
                         onChange={this.onValueChange}/>
-    
+                    </div>
                     <button type="submit"
-                            className="btn btn-outline-light">Добавить</button>
+                        className="btn btn-outline-light btn-lg">Добавить</button>
                 </form>
             </div>
         )
